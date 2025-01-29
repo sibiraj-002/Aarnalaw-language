784c65c86bc89a4e20ab29a5c771fd772eb804e4
Sibiraj Kandhasamy <sibirajkandhasamy@IVMBM2SibiRaj23.localdomain>
2025-01-29 11:23:36 +0530

    (live and staging) prodection updated in publication page

--- components/Publication/AllInsights.js
+++ components/Publication/AllInsights.js
@@ -1,46 +1,81 @@
 "use client";
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useCallback } from "react";
 import Link from "next/link";
+import configData from "../../config.json";
+import debounce from "lodash.debounce";
 
 function AllInsights({ searchTerm }) {
-  const [data, setData] = useState([]); // Initialize data state with an empty array
-  const [loading, setLoading] = useState(true); // Initialize loading state
-  const [hasMore, setHasMore] = useState(true); // Initialize hasMore state
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [page, setPage] = useState(6); // Start with 6 items
+  const [error, setError] = useState(null); // New state for error handling
+
+  const domain = typeof window !== "undefined" ? window.location.hostname : "";
+
+  const fetchContent = useCallback(async () => {
+    setLoading(true);
+    setError(null); // Reset the error state before each fetch
+    
+    try {
+      let server;
+      if (
+        domain === `${configData.LIVE_SITE_URL}` ||
+        domain === `${configData.LIVE_SITE_URL_WWW}`
+      ) {
+        server = `${configData.LIVE_PRODUCTION_SERVER_ID}`;
+      } else if (domain === `${configData.STAGING_SITE_URL}`) {
+        server = `${configData.STAG_PRODUCTION_SERVER_ID}`;
+      } else {
+        server = `${configData.STAG_PRODUCTION_SERVER_ID}`;
+      }
+
+      const publicationsResponse = await fetch(
+        `${configData.SERVER_URL}publications?_embed&status[]=publish&production_mode[]=${server}`,
+      );
+
+      if (!publicationsResponse.ok) {
+        throw new Error("Failed to fetch data");
+      }
+
+      const publicationsData = await publicationsResponse.json();
+      const sortedData = publicationsData.sort(
+        (a, b) => new Date(b.date) - new Date(a.date),
+      );
+      setData(sortedData);
+      setLoading(false);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+      setLoading(false);
+      setError("Something went wrong. Please try again later.");
+    }
+  }, [domain]);
 
   useEffect(() => {
-    const fetchData = async () => {
-      setLoading(true); // Start loading
-      try {
-        const response = await fetch(
-          `https://docs.aarnalaw.com/wp-json/wp/v2/publications?_embed&per_page=100`
-        );
-        const result = await response.json();
+    fetchContent();
+  }, [fetchContent]);
 
-        // Ensure the response is an array before setting the data
-        if (Array.isArray(result)) {
-          // Sort the data by date in descending order (newest to oldest)
-          const sortedData = result.sort((a, b) => new Date(b.date) - new Date(a.date));
-          setData(sortedData);
-        } else {
-          console.error("Expected an array but got:", result);
-        }
-      } catch (error) {
-        console.error("Error fetching data:", error);
-      } finally {
-        setLoading(false); // Stop loading
-      }
-    };
-
-    fetchData();
-  }, []);
+  const loadMore = () => {
+    if (data.length > page) {
+      setPage((prevPage) => prevPage + 6);
+    }
+  };
 
   const formatDateString = (dateString) => {
     const date = new Date(dateString);
     const monthAbbreviations = [
-      "JAN", "FEB", "MAR", "APR", "MAY", "JUN",
-      "JUL", "AUG", "SEP", "OCT", "NOV", "DEC",
+      "JAN",
+      "FEB",
+      "MAR",
+      "APR",
+      "MAY",
+      "JUN",
+      "JUL",
+      "AUG",
+      "SEP",
+      "OCT",
+      "NOV",
+      "DEC",
     ];
-
     return (
       <div className="flex flex-row items-center gap-2 lg:flex-col lg:gap-0">
         <p className="text-2xl font-bold text-custom-red">{date.getDate()}</p>
@@ -68,16 +103,22 @@ function AllInsights({ searchTerm }) {
     </div>
   );
 
-  const filteredInsights = data.filter((item) =>
-    item.title.rendered.toLowerCase().includes(searchTerm.toLowerCase())
-  );
+  const filteredInsights = data
+    .filter((item) =>
+      item.title.rendered.toLowerCase().includes(searchTerm.toLowerCase()),
+    )
+    .slice(0, page); // Show only up to the current page count
 
   return (
     <div className="mx-auto grid w-11/12 gap-4 py-12 lg:grid-cols-2">
       {loading && filteredInsights.length === 0 ? (
-        Array.from({ length: 8 }).map((_, index) => (
+        Array.from({ length: 6 }).map((_, index) => (
           <SkeletonLoader key={index} />
         ))
+      ) : error ? (
+        <div className="col-span-1 mt-4 text-center text-red-500 md:col-span-2">
+          {error}
+        </div>
       ) : filteredInsights.length > 0 ? (
         filteredInsights.map((item, index) => (
           <div
@@ -89,11 +130,11 @@ function AllInsights({ searchTerm }) {
             </div>
             <div className="p-5">
               <h5
-                className="mb-2 text-xl font-medium tracking-tight text-gray-900 dark:text-white sm:text-xl min-h-14 line-clamp-2"
+                className="mb-2 line-clamp-2 min-h-14 text-xl font-medium tracking-tight text-gray-900 dark:text-white sm:text-xl"
                 dangerouslySetInnerHTML={{ __html: item.title.rendered }}
               />
               <p
-                className="mb-3 font-normal text-gray-700 dark:text-gray-400 min-h-20"
+                className="mb-3 min-h-20 font-normal text-gray-700 dark:text-gray-400"
                 dangerouslySetInnerHTML={{
                   __html: stripHTMLAndLimit(item.content.rendered),
                 }}
@@ -104,6 +145,19 @@ function AllInsights({ searchTerm }) {
               >
                 Read more
               </Link>
+
+              {/* <Link
+                href={
+                  item.publication_url 
+                    ? item.publication_url
+                    : `/publications/${item.slug}`
+                }
+                className="font-semibold text-custom-red"
+                target={item.publication_url ? "_blank" : "_self"} // Open external links in a new tab
+                rel={item.publication_url ? "noopener noreferrer" : ""}
+              >
+                Read more
+              </Link> */}
             </div>
           </div>
         ))
@@ -113,10 +167,10 @@ function AllInsights({ searchTerm }) {
         </div>
       )}
 
-      {!loading && hasMore && filteredInsights.length > 0 && (
+      {!loading && data.length > page && (
         <div className="col-span-1 mt-6 flex justify-center md:col-span-2">
           <button
-            onClick={() => setHasMore(false)} // Adjust to implement a real load more function
+            onClick={loadMore}
             className="bg-custom-red px-4 py-2 text-white"
           >
             Load More
@@ -124,7 +178,7 @@ function AllInsights({ searchTerm }) {
         </div>
       )}
 
-      {!hasMore && (
+      {!loading && data.length <= page && (
         <div className="col-span-1 mt-4 text-center text-gray-500 md:col-span-2">
           No more details available
         </div>
