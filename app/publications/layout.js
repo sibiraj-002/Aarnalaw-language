b5d27f367d96f4af0edff7a0858b1dc86b6613d3
Sibiraj Kandhasamy <sibirajkandhasamy@IVMB-M2-Sibi-Raj-23.local>
2025-02-11 22:45:08 +0530

    testing

---
+++ app/publications/layout.js
@@ -0,0 +1,26 @@
+export const metadata = {
+    title: "Legal Insights and Expertise",
+    description:
+      "Stay informed with the latest legal insights and expert analyses across diverse practice areas. Explore Aarna Law's thought leadership and stay ahead in the legal landscape.",
+    metadataBase: new URL("https://www.aarnalaw.com"),
+    alternates: {
+      canonical: "/insights",
+    },
+    openGraph: {
+      title: "Legal Insights and Expertise",
+      description:
+        "Stay informed with the latest legal insights and expert analyses across diverse practice areas. Explore Aarna Law's thought leadership and stay ahead in the legal landscape.",
+      url: "https://www.aarnalaw.com/insights",
+      images: "/insights/InsightsBanner.jpg",
+    },
+  };
+
+export default function InsightsLayout({ children }) {
+    return <>{children}</>;
+  }
+
+
+
+//   export default function InsightsLayout({ children }) {
+//     return <>{children}</>;
+//   }
--- app/publications/page.js
+++ app/publications/page.js
@@ -1,30 +1,53 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
+import Banner from "@/components/Publication/Banner";
+import AllInsights from "@/components/Publication/AllInsights";
+import Navigation from "@/components/InsightsNavigation/Navigation";
 
-import InsightsClient from "./InsightsClient";
+export default function AarnaPublication() {
+  const [searchTerm, setSearchTerm] = useState("");
+  return (
+    <div>
+      <Banner title="publication" />
+      <Navigation searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+      <AllInsights searchTerm={searchTerm} />
+    </div>
+  );
+}
+
+
+
+
+
+// import React from "react";
+
+// import InsightsClient from "./InsightsClient";
+
+
+
+
+// export const metadata = {
+//   title: "Legal Publications and Research",
+//     description:
+//       "Access a comprehensive collection of legal publications and research papers authored by Aarna Law's experts. Stay up-to-date with cutting-edge legal scholarship.",
+//     metadataBase: new URL("https://www.aarnalaw.com"),
+//     alternates: {
+//       canonical: "/publications",
+//     },
+//     openGraph: {
+//       title: "Legal Publications and Research",
+//       description:
+//         "Access a comprehensive collection of legal publications and research papers authored by Aarna Law's experts. Stay up-to-date with cutting-edge legal scholarship.",
+//       url: "/publications",
+//        images: "/insights/InsightsBanner.jpg",
+//     },
+// };
+
+// export default function AarnaInsightsPage() {
+//   return <InsightsClient />;
+// }
 
 // import Seo from '@/components/SeoComponents/Seo'
-
-
-export const metadata = {
-  title: "Legal Publications and Research",
-    description:
-      "Access a comprehensive collection of legal publications and research papers authored by Aarna Law's experts. Stay up-to-date with cutting-edge legal scholarship.",
-    metadataBase: new URL("https://www.aarnalaw.com"),
-    alternates: {
-      canonical: "/publications",
-    },
-    openGraph: {
-      title: "Legal Publications and Research",
-      description:
-        "Access a comprehensive collection of legal publications and research papers authored by Aarna Law's experts. Stay up-to-date with cutting-edge legal scholarship.",
-      url: "/publications",
-       images: "/insights/InsightsBanner.jpg",
-    },
-};
-
-export default function AarnaInsightsPage() {
-  return <InsightsClient />;
-}
 
 // const AarnaInsightsPage = () => {
 //   const title = "Legal Publications and Research"
@@ -45,3 +68,6 @@ export default function AarnaInsightsPage() {
 // }
 
 // export default AarnaInsightsPage
+
+
+
